6694fbbbd07f32d9f66800eee41a19d6c4685faf
euijinbang <euijin.bang@gmail.com>
2022-07-02 13:41:27 +0900

    Upload: 0702

---
+++ project/src/비트연산자/BitTest.java
@@ -0,0 +1,24 @@
+package 비트연산자;
+
+public class BitTest {
+
+    public static void main(String[] args) {
+
+        int num1 = 5;   // 00000101
+        int num2 = 10;  // 00001010
+
+        System.out.println(num1 | num2);
+        System.out.println(num1 & num2);
+        System.out.println(num1 ^ num2);
+        System.out.println(~num1);
+
+        System.out.println(num1 << 1); // x2
+        System.out.println(num1);
+
+        System.out.println(num1 << 2); // x4
+        System.out.println(num1);
+
+        System.out.println(num1 <<= 2);
+        System.out.println(num1);
+    }
+}
